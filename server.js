07c48798387d3fbab047009e958e738b1bbbdfc1
bijancfarahani <bijancfarahani@gmail.com>
2017-01-03 23:50:39 -0800

    Added basic CSS

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.static('public'))
+app.use(express.static('styles'))
 
 const MongoClient = require('mongodb').MongoClient
 var db
