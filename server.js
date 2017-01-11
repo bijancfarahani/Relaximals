6bd60b1fdb86701922502c8ddde029ae3162f87b
bijancfarahani <bijancfarahani@gmail.com>
2017-01-10 16:46:54 -0800

    basic login, bugs with mongoose version present

--- server.js
+++ server.js
@@ -1,65 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const imageSearch = require("google-image-search-url-results");
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-app.use(express.static('public'))
-app.use(express.static('styles'))
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
 
-const MongoClient = require('mongodb').MongoClient
-var db
-MongoClient.connect('mongodb://hi:hi@ds145188.mlab.com:45188/relaximals', (err, database) => {
-  if (err) return console.log(err)
-  db = database
-  app.listen(3000, () => {
-  	console.log('listening on 3000')
-  })
-})
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 
-app.post('/quotes', (req, res) => {
-  db.collection('quotes').save(req.body, (err, result) => {
-    if (err) return console.log(err)
+var configDB = require('./config/database.js');
 
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
-app.get('/', (req, res) => {
-  db.collection('quotes').find().toArray((err, result) => {
-    if (err) return console.log(err)
-      res.render('index.ejs') 
-    })
-})
-app.post('/animal',(req, res) => {
-  var url;
-  imageSearch(req.body.animal, function(images) {
-    url = images[Math.floor(Math.random() * images.length)]
-    res.json(url);
-  })
-})
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
 
-app.put('/quotes', (req, res) => {
-  db.collection('quotes')
-  .findOneAndUpdate({name: 'Bijan'}, {
-    $set: {
-      name: req.body.name,
-      quote: req.body.quote
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-})
+require('./config/passport')(passport); // pass passport for configuration
 
-app.delete('/quotes', (req, res) => {
-  db.collection('quotes').findOneAndDelete({name: req.body.name},
-  (err, result) => {
-    if (err) return res.send(500, err)
-    res.json('A darth vadar quote got deleted')
-  })
-})
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
