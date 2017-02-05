fea2fe36a167db5e0fcf38b5b368263a3141079d
bijancfarahani <bijancfarahani@gmail.com>
2017-02-05 02:01:44 -0800

    more work on gallary/adding

--- public/app.js
+++ public/app.js
@@ -1,9 +1,17 @@
-var app = angular.module('animalApp', ['addAnimalCtrl',  'ngRoute', 'angular-filepicker'])
+    var app = angular.module('relaximalsApp', ['addAnimalCtrl', 'galleryCtrl', 'ngRoute', 'angular-filepicker'])
     .config(function($routeProvider, filepickerProvider){
         //The route provider handles the client request to switch route
         $routeProvider.when('/addAnimal', {          
             templateUrl: 'partials/addAnimal.html',
-                        controller: 'addAnimalController'            
+            controller: 'addAnimalController'            
+        })
+        .when('/gallery', {
+            templateUrl: 'partials/gallery.html',
+            controller: 'galleryController'
+        })
+         .when('/detail/:id', {
+            templateUrl: 'partials/detail.html',
+            controller: 'detailController'
         })
         //Redirect to addSuperhero in all the other cases.
         .otherwise({redirectTo:'/addAnimal'});
